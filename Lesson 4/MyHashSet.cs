a36a580edd59efa2b341ffe0ffdb5a0f77a3e708
AlxDmk <alxdmk@gmail.com>
2021-10-14 20:13:52 +0300

    ДЗ к Уроку 4

---
+++ Lesson 4/MyHashSet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class MyHashSet
+    {
+        public int Size { get; set; }
+        HashSet<int> SHashSet { get; set; }
+        
+
+        public MyHashSet(int size)
+        {
+            Size = size;
+            
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public void Fill()
+        {
+            
+
+            for (int i = 0; i < Size; i++)
+            {
+                int item = new Data().GetHashCode();
+                SHashSet.Add(item);
+            }
+        }
+
+        public bool FindItem(int item)
+        {
+            return SHashSet.Contains(item) ?true : false;
+            
+        }
+    }
+}
---
+++ Lesson 4/SimpleArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class SimpleArray
+    {
+        
+
+        int Size { get; set; }
+        string[] MyArray = new string [0];
+
+        public SimpleArray(int count)
+        {
+            Size = count;
+            Array.Resize(ref MyArray, MyArray.Length + Size);
+        }
+
+
+         public void Filling ()
+        {  
+           for (int i = 0; i < Size; i++)
+            {
+                MyArray[i] = new Data().ToString();
+
+            }          
+        } 
+
+        public bool FindItem (string item)
+        {
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                if (MyArray[i]== item)
+                {
+                    return true;                    
+                }                
+            }
+            return false;
+        }
+
+    }
+}

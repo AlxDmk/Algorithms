b3bff7d30a11d2da8f4d24e654cf88ea17483129
AlxDmk <alxdmk@gmail.com>
2021-10-17 13:24:23 +0300

    Задание к Уроку 5

---
+++ Lesson 5/NodeTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Lesson_5
+{
+    public class NodeTree
+    {
+        public Node Root { get; set; }
+
+        public void Add( int data)
+        {                       
+            if (Root == null)
+            {
+                Root = new(data);
+                return ;
+            }
+
+            Root.Add(data);
+
+            
+
+        }        
+        
+    }
+}

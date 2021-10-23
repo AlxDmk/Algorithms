7e01f85af31fe796d45ac90baf30094f467d6ee0
AlxDmk <alxdmk@gmail.com>
2021-10-23 21:39:56 +0300

    ДЗ к Уроку 7

---
+++ Lesson 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Algorithms.Lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrixMethod king = new MatrixMethod(8,8); 
+            king.Print();
+
+            LineMethod line = new LineMethod(8,8);
+            line.Print();
+            
+
+        }
+    }
+}
